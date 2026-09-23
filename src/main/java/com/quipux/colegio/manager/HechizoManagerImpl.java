64ad995de0065fbce2bd08f0309ba495849f6a60
nico25-code <oliverosgavirianicol@gmail.com>
2026-09-23 17:24:14 -0500

    Add Spring annotations for service and transaction management

--- src/main/java/com/quipux/colegio/manager/HechizoManagerImpl.java
+++ src/main/java/com/quipux/colegio/manager/HechizoManagerImpl.java
@@ -11,7 +11,8 @@ import java.util.List;
 // RETO 3: Agrega las anotaciones de Spring Boot para que esta clase sea un Servicio de Lógica
 // y para que los métodos se ejecuten dentro de una transacción de base de datos.
 // PISTAS: @S... y @T...
-
+@Service 
+@Transactional
 public class HechizoManagerImpl implements HechizoManager {
 
     @Autowired
@@ -21,8 +22,13 @@ public class HechizoManagerImpl implements HechizoManager {
     public HechizoEntity registrarHechizo(HechizoEntity hechizo) throws Exception {
         // RETO 3.1: Validar Reglas Mágicas
         // 1. Si el nombre del hechizo es nulo o está vacío, debes lanzar una Exception con el mensaje "Nombre invalido".
+        if (hechizo.getNombre() == null || hechizo.getNombre().trim().isEmpty()) {
+            throw new Exception("Nombre invalido");
+        }
         // 2. Si el "tipoMagia" del hechizo es "Oscura", debes lanzar una Exception con el mensaje "Magia prohibida en el colegio".
-        
+        if (hechizo.getTipoMagia() != null && hechizo.getTipoMagia().equals("Oscura")){
+            throw new Exception("Magia prohibida en el colegio");
+        }
         // Escribe tu código aquí:
         
         
